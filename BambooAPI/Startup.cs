e17690554767fa4ba9ee48ff790123c5fafc57a9
Peter Hsu <pthsu@outlook.com>
2017-12-15 11:41:46 +0800

    完成BambooAPI

--- BambooAPI/Startup.cs
+++ BambooAPI/Startup.cs
@@ -23,17 +23,22 @@ namespace BambooAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = "http://localhost:5000";
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = "api1";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
